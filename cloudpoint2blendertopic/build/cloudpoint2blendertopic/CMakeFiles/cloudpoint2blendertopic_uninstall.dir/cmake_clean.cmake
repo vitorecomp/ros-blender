23aa3ef13cc7316bb4279479cbdb9a7073339dc3
vitorecomp <vitor.ecomp@gmail.com>
2023-05-29 10:27:23 -0300

    start the plugins

---
+++ cloudpoint2blendertopic/build/cloudpoint2blendertopic/CMakeFiles/cloudpoint2blendertopic_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cloudpoint2blendertopic_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/cloudpoint2blendertopic_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cloudpoint2blendertopic/build/cloudpoint2blendertopic/CMakeFiles/cloudpoint2blendertopic_uninstall.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for cloudpoint2blendertopic_uninstall.
+# This may be replaced when dependencies are built.
