fc5eb1be9b956fea4775a2f5f85d0db083de8c8f
vitorecomp <vitor.ecomp@gmail.com>
2023-05-29 10:30:04 -0300

    feat: remove build folder

--- cloudpoint2blendertopic/build/cloudpoint2blendertopic/CMakeFiles/cloudpoint2blendertopic_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/cloudpoint2blendertopic_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/cloudpoint2blendertopic_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- cloudpoint2blendertopic/build/cloudpoint2blendertopic/CMakeFiles/cloudpoint2blendertopic_uninstall.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for cloudpoint2blendertopic_uninstall.
-# This may be replaced when dependencies are built.
